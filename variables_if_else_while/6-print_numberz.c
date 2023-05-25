10eef5bcf3fb75d72beb68d8f039d6a012dc5b55
mattkrozel <mattkrozel@gmail.com>
2023-05-25 09:01:56 -0700

    please work!

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
