892e4e9d77f94da0db762a49cf5f47d9e3203fc3
mattkrozel <mattkrozel@gmail.com>
2023-05-25 09:46:27 -0700

    please work!

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char l, n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (l = 'a'; l <= 'f'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+
+	return (0);
+}
