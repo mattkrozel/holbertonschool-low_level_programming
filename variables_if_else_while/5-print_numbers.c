090d688acae50a49d9297e1edcbc9081d43c43ba
mattkrozel <mattkrozel@gmail.com>
2023-05-25 08:45:48 -0700

    please work!

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		printf('%d'), n;
+		(n++);
+	}
+
+	putchar('\n');
+	return (0);
+}
