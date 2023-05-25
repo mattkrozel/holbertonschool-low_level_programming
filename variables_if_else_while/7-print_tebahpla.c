778521d0f3e4f8f55683523e53abf504ed7ae9e2
mattkrozel <mattkrozel@gmail.com>
2023-05-25 09:14:53 -0700

    please work!

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 'z'; l <= 'a'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+
+	return (0);
+}
