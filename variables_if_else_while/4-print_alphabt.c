299850452c8b2ab3f9709b2765155c3975a30f06
mattkrozel <mattkrozel@gmail.com>
2023-05-25 08:21:13 -0700

    please work!

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++ | grep[eq])
+	{
+		putchar(l);
+	}
+	putchar('\n');
+
+	return (0);
+}
