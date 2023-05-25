b969033c92364bdc1bb0321f604f6466bcd7235e
mattkrozel <mattkrozel@gmail.com>
2023-05-25 07:56:31 -0700

    please work!

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -7,15 +7,14 @@
  *   *
  *    * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char l
+	char l;
 
 	for (l = 'a'; l <= 'z'; l++)
-	{
 		putchar(l);
 		putchar('\n');
-	}
 
 	return (0);
 }
