075fe189a51bc2cad5d9ad6a753b24e0d986f240
mattkrozel <mattkrozel@gmail.com>
2023-05-25 07:37:46 -0700

    please work!

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int l;
+
+	for (int l = 'a'; l <= 'z'; l++)
+	{
+		putchar(tolower(l'\n'));
+	}
+
+	return (0);
+}
