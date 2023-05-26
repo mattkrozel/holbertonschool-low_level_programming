fe76e6a208faf82bddf5e3990ffb46b374b35312
mattkrozel <mattkrozel@gmail.com>
2023-05-26 09:26:23 -0700

    please work

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - Check main
+ * @n: An input number
+ * Description: function prints the sign of the number
+ * Return: 1, 0, or -1
+ */
+int print_sign(int n)
+{
+	int result = 0, num = 0;
+
+	if (n > 0)
+	{
+		result = 1;
+		printf("+");
+	}
+	else if (n < 0)
+	{
+		result = -1;
+		printf("-");
+	}
+	else
+	{
+		printf("0");
+	}
+	return (result);
+}
