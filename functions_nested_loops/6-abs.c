5cc0dae3601997fe59ae21edd2736899a8693c16
mattkrozel <mattkrozel@gmail.com>
2023-05-26 11:10:48 -0700

    please work

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - Check main
+ * @r: An input number
+ * Description: function that computes the absolute value of an integer
+ * Return: the absolute value
+ */
+int _abs(int r)
+{
+	int val;
+
+	val = abs(r);
+	return (val);
+}
