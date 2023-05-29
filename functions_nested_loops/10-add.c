99c38a7d69e60283c500d86eefe672c541117eaa
mattkrozel <mattkrozel@gmail.com>
2023-05-29 09:46:17 -0700

    please work please!

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * add - Check main
+ * @int: An input number
+ * Description: function that adds two integers
+ * Return: result
+ */
+int add(int, int)
+{
+	int result
+
+	result = int + int;
+
+	return (result);
+}
