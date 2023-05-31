1d290abf0c3028e1f7b619cbba5dbc5ddfe49339
mattkrozel <mattkrozel@gmail.com>
2023-05-31 10:39:32 -0700

    please work today

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - Check main
+ * int c
+ * Description: function prints from 0-9 except for 2 and 4
+ * Return: results
+ */
+
+void print_most_numbers(void)
+{
+	int c = '0';
+
+	for (; c <= '9'; c++)
+		if (c != '2' || c != '4')
+			_putchar(c);
+	_putchar('\n');
+}
