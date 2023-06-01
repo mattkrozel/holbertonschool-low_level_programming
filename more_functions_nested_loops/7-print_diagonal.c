8edc483ddc6529d34f10a29a067aabe657f96f14
mattkrozel <mattkrozel@gmail.com>
2023-06-01 10:18:30 -0700

    please work!

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,15 +10,15 @@
  */
 void print_diagonal(int n)
 {
-	int i = 0, j = 0;
+	int i, j;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
-			for (; j < n; j++)
+			for (j = 0; j < n; j++)
 			{
 				if (j == i)
 				{
