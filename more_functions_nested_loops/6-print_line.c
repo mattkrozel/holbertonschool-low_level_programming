42417a0c74eff8658ff97bcc346ccc820a96c3c8
mattkrozel <mattkrozel@gmail.com>
2023-06-01 08:19:00 -0700

    please work!

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - Check main
+ * @n: An input number
+ * Description: function that draws a straight line in the terminal
+ * Return: line
+ */
+void print_line(int n);
+{
+	while (n > 0)
+		_putchar(n * ('_'));
+		_putchar('\n');
+	else
+		_putchar('\n');
+	return (0);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -7,11 +7,5 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
+void print_line(int n);
 #endif
