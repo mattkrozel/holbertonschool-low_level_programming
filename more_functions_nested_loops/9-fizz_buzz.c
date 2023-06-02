de318e46d6e23b65801a116138a2b6c63e5218fe
mattkrozel <mattkrozel@gmail.com>
2023-06-02 09:53:51 -0700

    please work

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,19 +8,18 @@
  */
 int main(void)
 {
-	int x;
+	int x = 1;
 
-	for (x = 1; x <= 100; x++)
+	for (; x <= 100; x++)
 	{
-		if (x / 15)
+		if (x % 3 == 0 && x % 5 == 0)
 			printf("FizzBuzz ");
-		else if (x / 3)
+		else if (x % 3 == 0)
 			printf("Fizz ");
-		else if (x / 5)
+		else if (x % 5 == 0)
 			printf("Buzz ");
 		else
 			printf("%d ", x);
-		x++;
 	}
 	return (0);
 }
