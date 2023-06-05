c2d59bbae43bc9f0dba3ee6abd597dfe9686b46f
mattkrozel <mattkrozel@gmail.com>
2023-06-05 09:03:49 -0700

    git push

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -3,14 +3,12 @@
 #include "main.h"
 
 /**
- * mul - Check main
- * @a: An input number
- * @b: an inout number
+ * _strlen - Check main
+ * @s: An input number
  * Description: function that multiplies two integers
  * Return: result
  */
-int mul(int a, int b)
+int _strlen(char *s)
 {
-
-	return (a * b);
+	strlen(s);
 }
