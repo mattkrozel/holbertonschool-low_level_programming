aaa564bfbc3ac240e8154faab481cb1d1f193f66
mattkrozel <mattkrozel@gmail.com>
2023-06-05 09:28:25 -0700

    git push

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _puts - check the code
+ * @str: input
+ * Description: prints string followed by new line to stdout
+ * Return: return 0
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
