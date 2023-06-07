15d920bdb9717bb2b7dd29d0ed400d444d0b3579
mattkrozel <mattkrozel@gmail.com>
2023-06-07 08:09:15 -0700

    push to github

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * *_strcat - check the code
+ * @dest : input
+ * @src : input
+ * Description: concatenates two strings
+ * Return: total
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *temp = dest;
+
+	while (*dest)
+		dest++;
+
+	while (*src)
+		*dest++ = *src++;
+
+	return (temp);
+}
