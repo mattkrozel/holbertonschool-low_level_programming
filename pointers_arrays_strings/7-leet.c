d6a73de81f446ca4c49fbac46c0cf655bdeb2951
mattkrozel <mattkrozel@gmail.com>
2023-06-09 09:20:47 -0700

    push to github

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * *leet - Check main
+ * @s: input
+ * Description: encodes a string into 1337 replacing some letters with numbers
+ * Return: results
+ */
+
+char *leet(char *s)
+{
+	int l = 0, d = 0;
+	char a1[] = "AaEeOoTtLl";
+	char a2[] = "4433007711";
+
+	while (s[l])
+	{
+		if (s[l] == a1[])
+		{
+			s[l] = a2[l];
+			l++;
+		}
+		else
+			l++;
+	}
+	return (s);
+}
