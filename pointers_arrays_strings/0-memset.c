29f21d3e29a4b1891bf827291ffd4a322cede8b5
mattkrozel <mattkrozel@gmail.com>
2023-06-12 09:53:13 -0700

    push to github

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * *_memset - check the code
+ * @s : input
+ * @b : input
+ * @n : input
+ * Description: fills memory of s with b n times
+ * Return: total
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ret = s;
+	int i = 0;
+
+	for (; s[i] <= n ; i++)
+		s[i] = b;
+
+/*	*dest = '\0';	*/
+	return (ret);
+}
