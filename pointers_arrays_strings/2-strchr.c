65e1d8208cd158baf2827b7723b257b08d257df3
mattkrozel <mattkrozel@gmail.com>
2023-06-12 10:45:14 -0700

    push to github

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * *_strchr - check the code
+ * @s : input
+ * @c : input
+ * Description: locates character c in string s
+ * Return: total
+ */
+char *_strchr(char *s, char c)
+{
+	char *ret = c;
+	int i = 0;
+
+	for (; i ; i++)
+	{
+		if (s[i] == s[c])
+			return (ret);
+	}
+
+	return (0);
+}
