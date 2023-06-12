3869c9eb599cea613f89b61768a9c09625d3ab4e
mattkrozel <mattkrozel@gmail.com>
2023-06-12 11:44:44 -0700

    push to github

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _strspn - check the code
+ * @s : input
+ * @accept : input
+ * Description: gets length of a prefix substring
+ * Return: total
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int m = 0;
+
+	while (*s)
+	{
+		if (*accept == *s)
+			m++;
+		s++;
+	}
+
+	return (m);
+}
