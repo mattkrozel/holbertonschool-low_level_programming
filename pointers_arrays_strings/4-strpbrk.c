cf1c4255fed477d76ec12cfcd1c094ec32f43826
mattkrozel <mattkrozel@gmail.com>
2023-06-12 17:06:39 -0700

    push to github

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * *_strpbrk - check the code
+ * @s : input
+ * @accept : input
+ * Description: searches a string for any of a set of bytes
+ * Return: total
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	/*
+*	int x, y, ct = 0;
+*
+*	for (x = 0; s[x]; x++)
+*	{
+*		int match = 0;
+*
+*		for (y = 0; accept[y]; y++)
+*		{
+*			if (s[x] == accept[y])
+*			{
+*				match = 1;
+*				break;
+*			}
+*		}
+*		if (match == 0)
+*		{
+*			break;
+*		}
+*		ct++;
+*	}
+*
+*	return (ct);
+	*/
+	while (*s)
+	{
+		if (*accept == *s)
+			return (s);
+		s++;
+	}
+	if (*accept == *s)
+		return (s);
+	return (0);
+}
