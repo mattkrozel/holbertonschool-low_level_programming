0b41c5e0d7e094650ca32cb1ad5a37c712cb9f05
mattkrozel <mattkrozel@gmail.com>
2023-06-15 09:56:36 -0700

    push to github

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+int _sqrt_(int temp, sqrt);
+
+/**
+ * _sqrt_recursion - Check main
+ * @n: input
+ * Description: function that gets square root using recursion
+ * Return: results
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (_sqrt_(1, n));
+}
+
+/**
+ * _sqrt_ - check
+ * @temp: input
+ * @sqrt: input
+ * Return: Results
+ */
+
+int _sqrt_(int temp, sqrt)
+{
+	if (temp > sqrt)
+		return (-1);
+	else if (temp * temp == sqrt)
+		return (temp);
+	return (_sqrt_(temp + 1, sqrt));
+}
