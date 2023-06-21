9ab54c23bf3d77d1ffa6f4e8402390e6aa1e0dd6
mattkrozel <mattkrozel@gmail.com>
2023-06-21 08:33:41 -0700

    push to github

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -2,13 +2,14 @@
 #include "main.h"
 
 /**
- * main - a program that prints number of arguments passed to it
+ * main - a program that prints its name, followed by a new line
  * @argc: arguement count
+ * @argv: argument value, a strings that come after calling function
  * Return: Always 0 (Success)
  */
-int main(int argc)
+int main(int argc, char *argv[])
 {
-	if (argc > 0)
-		printf("%d\n", argc);
+	if (argv[0])
+		printf("%d\n", argc - 1);
 	return (0);
 }
