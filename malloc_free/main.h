b8aa4aa2ede67fd29c71455aa0f5018c7aa6133d
mattkrozel <mattkrozel@gmail.com>
2023-06-22 10:31:12 -0700

    push to github

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,5 +1,7 @@
 #ifndef _main_h
 #define _main_h_
+#include <stdio.h>
+#include <stdlib.h>
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
