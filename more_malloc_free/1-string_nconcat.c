8a9f38130aafb2f56ad896b4d8c7df68d1f6fbab
mattkrozel <mattkrozel@gmail.com>
2023-06-22 11:45:15 -0700

    push to github

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * *string_nconcat - function that concatenates two strings
+ * with very specific requirments
+ * @s1: input
+ * @s2: input
+ * @n: input
+ * Return: Nothing.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	
+}
