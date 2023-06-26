f0a6c007502e2aa993caf4a9739965fcb011feea
mattkrozel <mattkrozel@gmail.com>
2023-06-26 16:54:00 -0700

    push to github

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,51 +1,24 @@
 #include "main.h"
 
 /**
- * *_calloc - function that 
+ * *_calloc - function that allocates memory for an array using malloc
  * @size: input
  * @nmemb: input
  * Return: Nothing.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int x = 0, cnt1 = 0, cnt2 = 0;
-	char *finstr, *st1, *st2;
+	int x = 0;
+	char *space;
 
-	st1 = s1;
-	st2 = s2;
-	if (s1 == NULL)
-		s1 = "";
-	while (*s1)
-	{
-		cnt1++;
-		s1++;
-	}
-	s1 = st1;
-	if (s2 == NULL)
-		s2 = "";
-	while (*s2)
-	{
-		cnt2++;
-		s2++;
-	}
-	s2 = st2;
-	finstr = malloc(sizeof(char) * (cnt1 + cnt2 + 1));
-	st1 = finstr;
-	if (finstr == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	for (; x < (cnt1 + cnt2); x++)
-	{
-		if (x < cnt1)
-		{
-			finstr[x] = *s1;
-			s1++;
-		}
-		else
-		{
-			finstr[x] = *s2;
-			s2++;
-		}
-	}
-	finstr[x] = '\0';
-	return (st1);
+
+	space = malloc(nmemb * size);
+	if (space == NULL)
+		return (NULL);
+
+	for (; x < nmemb * size; x++)
+		*(space + x) = 0;
+	return (space);
 }
