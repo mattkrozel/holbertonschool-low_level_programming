3050400489b76c3eabf3bb82cac004067f97d87c
mattkrozel <mattkrozel@gmail.com>
2023-07-03 09:10:24 -0700

    push to github

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#endif
