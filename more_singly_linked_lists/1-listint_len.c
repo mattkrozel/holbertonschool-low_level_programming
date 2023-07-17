e95cae48c2e5a4344be7d659e17d19fba34957c3
mattkrozel <mattkrozel@gmail.com>
2023-07-17 09:13:29 -0700

    push tp github

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * list_len - a function that returns number of elements in linked list
+ * listint_len - a function that returns number of elements in linked list
  * @h: input
  * Return: nothing
  */
 
-size_t list_len(const list_t *h)
+size_t listint_len(const listint_t *h)
 {
 
-	unsigned int elements = 0;
+	size_t elements = 0;
 
 	while (h)
 	{
-		elements++;
 		h = h->next;
+		elements++;
 	}
 	return (elements);
 }
