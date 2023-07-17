7a68f2480a90fe8d32562dad8449936c32f6a026
mattkrozel <mattkrozel@gmail.com>
2023-07-17 09:24:38 -0700

    push tp github

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -1,22 +1,20 @@
 #include "lists.h"
 
 /**
- * free_list - frees all elements in linked list
+ * free_listint - frees all elements in linked list
  * @head: input
  * Return: nothing
  */
 
-void free_list(list_t *head)
+void free_listint(listint_t *head)
 {
 
-	list_t *temp;
+	listint_t *temp;
 
-	temp = head;
 	while (head)
 	{
 		temp = head;
 		head = head->next;
-		free(temp->str);
 		free(temp);
 	}
 	free(head);
