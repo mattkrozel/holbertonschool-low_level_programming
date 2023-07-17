242846568f7a59665ec75eb67fef7d69d10782b8
mattkrozel <mattkrozel@gmail.com>
2023-07-17 11:24:54 -0700

    push tp github

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -9,34 +9,26 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int x = 0;
-	listint_t *nnode, *temp;
+	listint_t *temp = *head;
+	listint_t *nnode = malloc(sizeof(listint_t));
 
-	nnode = malloc(sizeof(listint_t));
-	if (head == NULL || (nnode == NULL)
+	if (!nnode)
 		return (NULL);
 	nnode->n = n;
-	if (*head == NULL && idx == 0)
-	{
-		*head = nnode;
-		nnode->next = NULL;
-		return (nnode);
-	}
 	if (idx == 0)
 	{
-		nnode->next = *head;
+		nnode->next = temp;
 		*head = nnode;
 		return (nnode);
 	}
-	temp = *head;
-	while (temp != NULL && x < idx - 1)
+
+	while (x < (idx - 1))
 	{
+		if (temp == NULL || temp->next == NULL)
+			return (NULL);
+
 		temp = temp->next;
 		x++;
-	}
-	if (temp == NULL)
-	{
-		free(nnode);
-		return (NULL);
 	}
 	nnode->next = temp->next;
 	temp->next = nnode;
