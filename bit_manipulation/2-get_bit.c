59a20987f4efcacc8ea2aaac9914106822fe1c13
mattkrozel <mattkrozel@gmail.com>
2023-07-17 14:33:47 -0700

    push to github

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,28 +1,20 @@
-#include "lists.h"
+#include "main.h"
 
 /**
- * *add_node - a function that adds a new node at the beginning of list_t
- * @str: input
- * @head: head
- * Return: address of a new element or null if failed
+ * get_bit - gets value of bit at specific place
+ * @n: input
+ * @index: input
+ * Return: 1 or 0
  */
 
-list_t *add_node(list_t **head, const char *str)
+int get_bit(unsigned long int n, unsigned int index)
 {
 
-	int length = 0;
-	list_t *temp;
+	int x;
 
-	temp = malloc(sizeof(list_t));
-	if (temp == NULL)
-		return (NULL);
+	if (index > 63)
+		return (-1);
 
-	while (str[length])
-		length++;
-
-	temp->len = length;
-	temp->str = strdup(str);
-	temp->next = *head;
-	*head = temp;
-	return (temp);
+	x = n >> index;
+	return (x & 1);
 }
