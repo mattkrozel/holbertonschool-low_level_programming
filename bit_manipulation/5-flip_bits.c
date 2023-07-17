aa4eb878e5a4a12abf3e60d9ccaaa614426ddd98
mattkrozel <mattkrozel@gmail.com>
2023-07-17 14:53:18 -0700

    push to github

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,1 +1,22 @@
-#include 
+#include "main.h"
+
+/**
+ * flip_bits - returns number of bits you would need to flip to
+ * get from one to another
+ * @n: input
+ * @m: input
+ * Return: nothing
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+	unsigned long int comp = n ^ m;
+	unsigned int total = 0;
+
+	for (; comp > 0;)
+	{
+		total += (comp & 1);
+		comp >>= 1;
+	}
+	return (total);
