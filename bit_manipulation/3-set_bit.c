779395872d699139e3212c6124d3c0c5c37e8ad4
mattkrozel <mattkrozel@gmail.com>
2023-07-17 14:38:56 -0700

    push to github

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,39 +1,17 @@
-#include "lists.h"
+#include "main.h"
 
 /**
- * *add_node_end - a function that adds a new node at the end of list_t
- * @str: input
- * @head: head
- * Return: address of a new element or null if failed
+ * set_bit - sets value of specific bit
+ * @n: input
+ * @index: input
+ * Return: 1 or 0
  */
 
-list_t *add_node_end(list_t **head, const char *str)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 
-	unsigned int length = 0;
-	list_t *temp, *nnode;
-
-	nnode = malloc(sizeof(list_t));
-	if (nnode == NULL)
-		return (NULL);
-
-	while (str[length])
-		length++;
-
-	nnode->len = length;
-	nnode->str = strdup(str);
-	if (*head == NULL)
-	{
-		nnode->next = *head;
-		*head = nnode;
-	}
-	else
-	{
-		nnode->next = NULL;
-		temp = *head;
-		while (temp->next)
-			temp = temp->next;
-		temp->next = nnode;
-	}
-	return (nnode);
+	if (index > 63)
+		return (-1);
+	*n ^= (1 << index);
+	return (1);
 }
