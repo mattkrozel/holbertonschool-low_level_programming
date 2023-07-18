e324ec60f2ee94f4bed5b722cbd429b995fc72eb
mattkrozel <mattkrozel@gmail.com>
2023-07-17 17:21:10 -0700

    push to github

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,20 +10,21 @@
 int create_file(const char *filename, char *text_content)
 {
 
-	int openfile;
+	int open_file;
 
 	if (filename == NULL)
 		return (-1);
 
-	openfile = open(filename, O_WRONLY | O_CREAT | O_TRUNC | 0600);
-	if (openfile == -1)
+	open_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC | 0600);
+	if (open_file == -1)
 		return (-1);
 
 	if (text_content)
 	{
-		if (write(openfile, text_content, strlen(text_content)) == -1)
+		if (write(open_file, text_content, strlen(text_content)) == -1)
 			return (-1);
 	}
-	close(openfile);
+	close(open_file);
+	
 	return (1);
 }
