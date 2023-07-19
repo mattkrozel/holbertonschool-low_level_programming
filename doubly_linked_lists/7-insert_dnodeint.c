47520e960764d2d7afc4217484963d10e9de743c
mattkrozel <mattkrozel@gmail.com>
2023-07-19 09:47:20 -0700

    task 7 push to git

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,29 +12,44 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 
 	unsigned int x = 0;
-	dlistint_t *temp = *h;
-	dlistint_t *nnode = malloc(sizeof(dlistint_t));
+	dlistint_t *temp = NULL, *nnode = NULL;
+	
+	nnode = malloc(sizeof(dlistint_t));
 
 	if (!nnode)
 		return (NULL);
-
+	nnode->next = NULL;
+	nnode->prev = NULL;
 	nnode->n = n;
-	if (idx == 0)
-	{
-		nnode->next = temp;
+
+	if (!h || !(*h))
 		*h = nnode;
-		return (nnode);
-	}
-
-	while (x < (idx - 1))
+	else
 	{
-		if (temp == NULL || temp->next == NULL)
+		temp = *h;
+		while (idx != x++ && temp->next)
+			temp = temp->next;
+		if (temp->next)
+			nnode->prev = temp->prev;
+		else
+			nnode->prev = temp;
+		if (idx == x)
+			temp->next = nnode, nnode->prev = temp;
+		else if (idx == x - 1)
+		{
+			if (temp->prev)
+				temp->prev->next = nnode;
+			else
+				*h = nnode;
+			temp->prev = nnode;
+			nnode->next = temp;
+		}
+		else
+		{
+			free(nnode);
 			return (NULL);
-		temp = temp->next;
-		x++;
+		}
 	}
 
-	nnode->next = temp->next;
-	temp->next = nnode;
 	return (nnode);
 }
