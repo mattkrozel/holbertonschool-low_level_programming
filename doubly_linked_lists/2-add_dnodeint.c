a31bc869d3d9ca1e5e31fc0050bbd46368bb0f5b
mattkrozel <mattkrozel@gmail.com>
2023-07-19 08:40:44 -0700

    task 2 push

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *temp = NULL, *nnode = NULL;
 
 	nnode = malloc(sizeof(dlistint_t));
-	if (!*nnode)
+	if (!nnode)
 	{
 		free(nnode);
 		return (NULL);
@@ -31,7 +31,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	else
 	{
 		temp = *head;
-		temp->prev = nnnode;
+		temp->prev = nnode;
 		*head = nnode;
 		nnode->next = temp;
 		return (nnode);
