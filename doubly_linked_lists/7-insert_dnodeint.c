6cded052ae7003468a4d38d255b1e30dc3055c49
mattkrozel <mattkrozel@gmail.com>
2023-07-19 10:28:38 -0700

    task 7 push to git

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -7,15 +7,12 @@
  * @n: input
  * Return: nothing
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-
 	unsigned int x = 0;
 	dlistint_t *temp = NULL, *nnode = NULL;
-	
-	nnode = malloc(sizeof(dlistint_t));
 
+	nnode = malloc(sizeof(dlistint_t));
 	if (!nnode)
 		return (NULL);
 	nnode->next = NULL;
