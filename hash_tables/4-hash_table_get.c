374e3402d4bd2681023a6f7f881b0fb89e4446c6
mattkrozel <mattkrozel@gmail.com>
2023-07-24 15:03:53 -0700

    push to github task 0

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -18,7 +18,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	else if (strlen(key) == 0)
 		return (NULL);
 
-	index = key_index((unsigned char *)key, ht->size;
+	index = key_index((unsigned char *)key, ht->size);
 	if ((ht->array)[index] == NULL)
 		return (NULL);
 	node = ht->array[index];
