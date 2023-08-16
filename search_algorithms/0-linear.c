7f85b299ea2af7939ba4393945e340aca44d8b24
mattkrozel <mattkrozel@gmail.com>
2023-08-16 09:14:21 -0700

    edit compile issues

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -15,8 +15,11 @@ int linear_search(int *array, size_t size, int value)
 	if (array != NULL)
 	{
 		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%d] = [%d]\n", i, array[i]);
 			if (array[i] == value)
 				return (i);
+		}
 	}
 	return (-1);
 
