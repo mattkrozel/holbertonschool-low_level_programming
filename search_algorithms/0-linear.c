75d75558e6186f642ecb2d42c7f88c2b677343c2
mattkrozel <mattkrozel@gmail.com>
2023-08-16 08:44:55 -0700

    adding header file and task 0 from the first project

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searchs an array using linear algorithm
+ * @array: array
+ * @size: size of array
+ * @value: value we're checking
+ * Return: value or -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+
+	int i;
+
+	if (array != NULL)
+	{
+		for (i = 0; i < size; i++)
+			if (array[i] == value)
+				return (i);
+	}
+	else
+		return (-1);
+
+}
